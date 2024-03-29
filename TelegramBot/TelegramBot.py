eb172332ac2f9546123a4299d778cf72ae481af7
MironMiron753 <mironpython@mail.ru>
2024-03-29 18:16:39 +0500

    8 строка "Пока пока!" на "4 модуль начался!"

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -5,8 +5,9 @@ bot = telebot.TeleBot('7162103985:AAEimCQMejkVTqVvcyLul-nZTHVn8wWfOyA')
 @bot.message_handler(commands=['start'])
 
 def main(message):
-    bot.send_message(message.chat.id, '���� ����!')
+    bot.send_message(message.chat.id, '4 модуль начался!')
     bot.send_message('до встречи!')
     bot.send_message('До скорой встречи!')
+    
 
 bot.infinity_polling()
