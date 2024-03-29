614bbde64a376d9f5ede0585fc29c706351ba05a
Юрий Бояршинов <boyarshinov.yuriy@gmail.com>
2024-03-29 18:27:23 +0500

    удалено первое сообщение третье сообщение сокращено

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -5,9 +5,8 @@ bot = telebot.TeleBot('7162103985:AAEimCQMejkVTqVvcyLul-nZTHVn8wWfOyA')
 @bot.message_handler(commands=['start'])
 
 def main(message):
-    bot.send_message(message.chat.id, 'до свидания 3 модуль')
     bot.send_message(message.chat.id, 'до встречи!')
-    bot.send_message(message.chat.id, 'До скорой встречи!')
+    bot.send_message(message.chat.id, 'До встречи!')
     
 
 bot.infinity_polling()
