15ebd7b611a4e2cd72415d3e0bfb699793796abe
Леон Коссе <angryredbird100@gmail.com>
2024-03-29 15:37:27 +0300

    Изменил вывод на слово "Пока!"

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot('7162103985:AAEimCQMejkVTqVvcyLul-nZTHVn8wWfOyA')
 @bot.message_handler(commands=['start'])
 
 def main(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, 'Пока!')
 
 bot.infinity_polling()
