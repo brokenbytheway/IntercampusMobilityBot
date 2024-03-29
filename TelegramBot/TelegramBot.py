22b4c648c2418b7ad8856996ed90aa081db8b942
Юрий Бояршинов <boyarshinov.yuriy@gmail.com>
2024-03-29 18:28:34 +0500

    первое сообщение изменено

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('7162103985:AAEimCQMejkVTqVvcyLul-nZTHVn8wWfOyA')
 @bot.message_handler(commands=['start'])
 
 def main(message):
-    bot.send_message(message.chat.id, 'до встречи!')
+    bot.send_message(message.chat.id, 'До свидания!')
     bot.send_message(message.chat.id, 'До встречи!')
     
 
