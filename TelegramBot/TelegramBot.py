c37f0bfa40efe1a1d0e6c67e21fec0b5415aac0b
MironMiron753 <mironpython@mail.ru>
2024-03-29 18:07:38 +0500

    Изменил 8 строку.  "Пока!" на "Пока пока!"

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot('7162103985:AAEimCQMejkVTqVvcyLul-nZTHVn8wWfOyA')
 @bot.message_handler(commands=['start'])
 
 def main(message):
-    bot.send_message(message.chat.id, 'Bye!')
+    bot.send_message(message.chat.id, 'Пока пока!')
 
 bot.infinity_polling()
