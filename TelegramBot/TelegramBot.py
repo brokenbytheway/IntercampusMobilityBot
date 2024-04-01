dd374e19642fa9d47669376fbaff2f7c6d51ece3
Юрий Бояршинов <boyarshinov.yuriy@gmail.com>
2024-04-01 23:16:18 +0500

    Изменено: Уменьшены кнопки, изменено их расположение для более удобного восприятия. Добавлена команда "помощь", где пользователь видит информацию о работе бота. Доработан ввод направлений: направления отображаются в зависимости от указанного курса. Решена проблема своевременного исчезновения кнопок.

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -11,39 +11,61 @@ worksheet = sheet.sheet1
 # Массив для хранения данных анкеты
 form_data = []
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'back']) #главнок меню
 def start(message):
     # Приветственное сообщение и создание кнопки для заполнения анкеты
     bot.send_message(message.chat.id, 'Привет! Я бот для заполнения анкеты на межкампусную мобильность.')
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('Заполнить анкету')
-    markup.add(btn1)
-    bot.send_message(message.chat.id, 'Пожалуйста, нажмите на кнопку "Заполнить анкету" для начала заполнения анкеты.', reply_markup=markup)
-    bot.register_next_step_handler(message, select_course)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn1 = types.KeyboardButton('/form')
+    btn2 = types.KeyboardButton('/help')
+    markup.add(btn1).add(btn2)
+    bot.send_message(message.chat.id, 'Пожалуйста, нажмите на кнопку "/form" для начала заполнения анкеты. Для получения дополнительной информации нажмите на кнопку "/help"', reply_markup=markup)
 
+
+@bot.message_handler(commands=['form']) #заполнение анкеты
 def select_course(message):
-    if message.text == 'Заполнить анкету':
-        # Создание кнопок для выбора курса с 2 по 4
-        markup_course = types.ReplyKeyboardMarkup()
-        for course in range(2, 5):
-            button = types.KeyboardButton(course)
-            markup_course.add(button)
+    markup_course = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn1= types.KeyboardButton('1')
+    btn2= types.KeyboardButton('2')
+    btn3= types.KeyboardButton('3')
+    btn4= types.KeyboardButton('4')
+    markup_course.row(btn1,btn2)
+    markup_course.row(btn3,btn4)
+    bot.send_message(message.chat.id, 'Выберите ваш курс:', reply_markup=markup_course)
+    bot.register_next_step_handler(message, select_direction)
     
-        bot.send_message(message.chat.id, 'Выберите ваш курс:', reply_markup=markup_course)
-        bot.register_next_step_handler(message, select_direction)
     
 def select_direction(message):
     # Сохраняем выбранный пользователем курс
     form_data.append(message.text)
     
     # Создание кнопок для выбора направления
-    markup_direction = types.ReplyKeyboardMarkup()
-    directions = ['РИС', 'МБ', 'И', 'Ю', 'ИЯ']  # Пример направлений
-    for direction in directions:
-        button = types.KeyboardButton(direction)
-        markup_direction.add(button)
-    
-    bot.send_message(message.chat.id, 'Выберите ваше направление:', reply_markup=markup_direction)
+    markup_direction = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    if message.text == '1' or message.text == '2': #направления 1 и 2 курсов
+        p0 = types.KeyboardButton('Программная инженерия')
+        p1 = types.KeyboardButton('Бизнес информатика')
+        p2 = types.KeyboardButton('Междунарожный бакалавриат')
+        p3 = types.KeyboardButton('История')
+        p4 = types.KeyboardButton('Юриспруденция')
+        p5 = types.KeyboardButton('Иностранные языки')
+        markup_direction.row(p0,p1)
+        markup_direction.row(p2)
+        markup_direction.row(p3,p4)
+        markup_direction.row(p5)
+        bot.send_message(message.chat.id, 'Выберите ваше направление (Важно! Направление "Разработка инофрмационных систем для бизнеса" разделено на направления "Программная инженерия" и "Бизнес информатика" вследствие разных кодов.):', reply_markup=markup_direction)
+    if message.text == '3' or message.text == '4': #направления 3 и 4 курсов
+        p1 = types.InlineKeyboardButton('Программная инженерия')
+        p2 = types.InlineKeyboardButton('Бизнес информатика')
+        p3 = types.InlineKeyboardButton('История')
+        p4 = types.InlineKeyboardButton('Юриспруденция')
+        p5 = types.InlineKeyboardButton('Иностранные языки')
+        p6 = types.InlineKeyboardButton('Экономика')
+        p7 = types.InlineKeyboardButton('Управление бизнесом')
+        markup_direction.row(p1,p2)
+        markup_direction.row(p6,p7)
+        markup_direction.row(p3,p4)
+        markup_direction.row(p5)
+        bot.send_message(message.chat.id, 'Выберите ваше направление:', reply_markup=markup_direction)
     bot.register_next_step_handler(message, enter_lastname)
 
 def enter_lastname(message):
@@ -51,7 +73,7 @@ def enter_lastname(message):
     form_data.append(message.text)
     
     # Запрос фамилии
-    bot.send_message(message.chat.id, 'Введите вашу фамилию:')
+    bot.send_message(message.chat.id, 'Введите вашу фамилию:', reply_markup=types.ReplyKeyboardRemove())
     bot.register_next_step_handler(message, enter_firstname)
 
 def enter_firstname(message):
@@ -84,11 +106,10 @@ def confirm_data(message):
                         f"Данные верны?"
     
     # Создание кнопок для подтверждения данных
-    markup_confirmation = types.ReplyKeyboardMarkup()
-    buttons = ['Да', 'Нет']
-    for button in buttons:
-        markup_confirmation.add(button)
-    
+    markup_confirmation = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    yes = types.KeyboardButton('Да')
+    no = types.KeyboardButton('Нет')
+    markup_confirmation.row(yes,no)
     bot.send_message(message.chat.id, confirmation_text, reply_markup=markup_confirmation)
     bot.register_next_step_handler(message, process_confirmation)
 
@@ -96,12 +117,21 @@ def process_confirmation(message):
     # Обработка выбора пользователя
     if message.text.lower() == 'да':
         # Данные верны
-        bot.send_message(message.chat.id, 'Спасибо, ваша анкета успешно заполнена!')
+        bot.send_message(message.chat.id, 'Спасибо, ваша анкета успешно заполнена!', reply_markup=types.ReplyKeyboardRemove())
         worksheet.append_row([form_data[0], form_data[1], form_data[2], form_data[3], form_data[4]])
     elif message.text.lower() == 'нет':
         # Данные неверны
         bot.send_message(message.chat.id, 'Пожалуйста, введите данные заново.')
         form_data.clear()
         start(message)
+        
+@bot.message_handler(commands=['help']) #помощь
+def help(message):
+    help_info = 'Я бот для заполнения анкеты на межкампусную мобильность. Через меня ты сможешь подать заявку для участия в межкампусной мобильности. Для этого просто начни заполнять анкету. Когда ты выберешь свой курс и направление, я сам предложу тебе вараинты, в какой город и на какое направление ты сможешь отправиться. Помни, ты должен честно заполнять все данные! Особенно рейтинг! Иначе на мобильность не возьмём. :) Нажми кнопку "/form", чтоб приступить к заполнению анкеты. Кнопка "/back" возвращает в главное меню.'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn1 = types.KeyboardButton('/form')
+    btn2 = types.KeyboardButton('/back')
+    markup.add(btn1).add(btn2)
+    bot.send_message(message.chat.id, help_info, reply_markup=markup)
     
 bot.infinity_polling()
