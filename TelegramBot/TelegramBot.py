207f921eb156b43205ff9adcbcf39adefb95a7de
Юрий Бояршинов <boyarshinov.yuriy@gmail.com>
2024-04-03 18:29:58 +0500

    Добавил функция выбора мобильности: Пока только начал, описал только ПИ и БИ, остальные по логике можно сделать такими же. Кнопка, отсылающая на сайт, нормально работает, а вот кнопка для записи пока плохо реализована. Там надо, чтобы сообщения после этого удалялись, ну и как-то забить оставшиеся данные в таблицу.

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -1,4 +1,5 @@
-import select
+from atexit import register
+import re
 import telebot
 import gspread
 from telebot import types
@@ -62,15 +63,18 @@ def select_direction(message):
     if message.text == '1' or message.text == '2': #направления 1 и 2 курсов
         p0 = types.KeyboardButton('Программная инженерия')
         p1 = types.KeyboardButton('Бизнес информатика')
-        p2 = types.KeyboardButton('Междунарожный бакалавриат')
+        p2 = types.KeyboardButton('Экономика')
+        p6 = types.KeyboardButton('Менеджмент')
         p3 = types.KeyboardButton('История')
         p4 = types.KeyboardButton('Юриспруденция')
         p5 = types.KeyboardButton('Иностранные языки')
         markup_direction.row(p0,p1)
-        markup_direction.row(p2)
+        markup_direction.row(p2,p6)
         markup_direction.row(p3,p4)
         markup_direction.row(p5)
-        bot.send_message(message.chat.id, 'Выберите ваше направление (Важно! Направление "Разработка инофрмационных систем для бизнеса" разделено на направления "Программная инженерия" и "Бизнес информатика" вследствие разных кодов.):', reply_markup=markup_direction)
+        bot.send_message(message.chat.id, 'Выберите ваше направление:', reply_markup=markup_direction)
+        bot.send_message(message.chat.id, 'Важно! Направление "Разработка инофрмационных систем для бизнеса" разделено на направления "Программная инженерия" и "Бизнес информатика" вследствие разных кодов.')
+        bot.send_message(message.chat.id, 'Важно! Направление "Международный бакалавриат по бизнесу и экономике" разделено на направления "Экономика" и "Менеджмент" вследствие разных кодов.')
     if message.text == '3' or message.text == '4': #направления 3 и 4 курсов
         p1 = types.InlineKeyboardButton('Программная инженерия')
         p2 = types.InlineKeyboardButton('Бизнес информатика')
@@ -145,7 +149,7 @@ def process_confirmation(message):
     if message.text.lower() == 'да':
         # Данные верны
         worksheet.append_row([form_data[0], form_data[1], form_data[2], form_data[3], form_data[4]])
-        start(message)
+        mobility(message)
     elif message.text.lower() == 'нет':
         # Данные неверны
         bot.send_message(message.chat.id, 'Пожалуйста, введите данные заново.')
@@ -172,7 +176,55 @@ def next_command2(message): #переход на следующую команд
     else:
         help_inf(message)
  
-#@bot.message_handler(commands=['mobility']) #выбор мобильности
-#def mobility(message):
-    
+@bot.message_handler(commands=['mobility']) #выбор мобильности
+def mobility(message):
+    bot.send_message(message.chat.id, "Исходя из вашего направления, мы можем предложить вам следующие варианты межкампусной мобильности:", reply_markup=types.ReplyKeyboardRemove())
+    markupMoscow = types.InlineKeyboardMarkup() #области кнопок для каждого города
+    markupNovgorod = types.InlineKeyboardMarkup()
+    markupPetersburg = types.InlineKeyboardMarkup()
+    if form_data[1] == 'Программная инженерия':
+        siteMoscow = types.InlineKeyboardButton('Ознакомиться с программой', url = 'https://www.hse.ru/ba/se/')
+        siteNovgorod = types.InlineKeyboardButton('Ознакомиться с программой', url = 'https://nnov.hse.ru/bipm/se/')
+        registerMoscow = types.InlineKeyboardButton('Записаться', callback_data= 'pi_msk')
+        registerNovgorod = types.InlineKeyboardButton('Записаться', callback_data='pi_nn')
+        markupMoscow.row(siteMoscow,registerMoscow)
+        markupNovgorod.row(siteNovgorod,registerNovgorod)
+        bot.send_message(message.chat.id, 'Програмная инженерия \nМосква', reply_markup=markupMoscow)
+        bot.send_message(message.chat.id, 'Программная инженерия (очно-заочное обучение) \nНижний Новгород', reply_markup=markupNovgorod)
+    elif form_data[1] == 'Бизнес информатика':
+        siteMoscow = types.InlineKeyboardButton('Ознакомиться с программой', url = 'https://www.hse.ru/ba/bi/')
+        siteNovgorod = types.InlineKeyboardButton('Ознакомиться с программой', url = 'https://nnov.hse.ru/ba/cst/')
+        sitePetersburg = types.InlineKeyboardButton('Ознакомиться с программой', url = 'https://spb.hse.ru/ba/bi/')
+        markupMoscow.row(siteMoscow)
+        markupNovgorod.row(siteNovgorod)
+        markupPetersburg.row(sitePetersburg)
+        bot.send_message(message.chat.id, 'Бизнес информатика \nМосква', reply_markup=markupMoscow)
+        bot.send_message(message.chat.id, 'Компьютерные науки и технологии \nНижний Новгород', reply_markup=markupNovgorod)
+        bot.send_message(message.chat.id, 'Бизнес-информатика \nСанкт-Петербург', reply_markup=markupPetersburg)
+    elif form_data[1] == 'Экономика':
+        pass
+    elif form_data[1] == 'Менеджмент':
+        pass
+    elif form_data[1] == 'История':
+        pass
+    elif form_data[1] == 'Юриспруденция':
+        pass
+    elif form_data[1] == 'Иностранные языки':
+        pass
+    elif form_data[1] == 'Управление бизнесом':
+        pass
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call): #осуществление записи на мобильность, здесь нужно реализовать добавление данных о мобильности в таблицу
+    for i in range(-4,0):
+        bot.delete_message(call.chatid,call.messageid-1)
+    try:
+        if call.message:
+            if call.data == "pi_msk":
+                bot.send_message(call.message.chat.id, 'Вы успешно записались на обрзовательную программу "Програмная инженерия" в городе Москва!')
+            if call.data == "pi_nn":
+                bot.send_message(call.message.chat.id, 'Вы успешно записались на обрзовательную программу "Программная инженерия (очно-заочное обучение)" в городе Нижний Новгород!')
+    except Exception as e:
+        print(repr(e))
+    bot.send_message(call.message.chat.id, 'Или нет...')
 bot.infinity_polling()
